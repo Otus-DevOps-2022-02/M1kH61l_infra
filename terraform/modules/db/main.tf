f6d369d9727f6806d5f320d5cdad6cceb12e33d7
Mikhail Tekunov <ma.tekunov@gmail.com>
2022-04-23 21:43:59 +0300

    Terraform-2 Task

---
+++ terraform/modules/db/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+resource "yandex_compute_instance" "db" {
+  name = "reddit-db-terraform-2"
+
+  labels = {
+    tags = "reddit-db-terraform-2"
+  }
+  resources {
+    core_fraction = 5
+    cores         = 2
+    memory        = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.db_disk_image
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
+
